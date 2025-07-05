576b6fdfc44e7d513f20c12bcdfa2fc5753a78fc
kruegersruh <kruegersruh@mailbox.org>
2025-07-06 01:33:29 +0200

    Commit.

--- src/content.config.mjs
+++ src/content.config.mjs
@@ -16,15 +16,16 @@ const albums = defineCollection({
     }),
 });
 
-const projects = defineCollection({
-  loader: glob({ pattern: '**/*.mdx', base: './src/content/projects' }),
+const posts = defineCollection({
+  loader: glob({ pattern: '**/*.mdx', base: './src/content/posts' }),
   schema: z.object({
     title: z.string(),
     author: z.string(),
     description: z.string(),
+    cover: z.string(),
   }),
 })
 
 
 // 4. Export a single `collections` object to register you collection(s)
-export const collections = { albums, projects }
+export const collections = { albums, posts }
